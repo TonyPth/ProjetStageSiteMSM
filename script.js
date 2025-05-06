843aeec6af767097613a5ced3b3b25989f2d8c4f
TonyPth <tony.ponthieu@outlook.com>
2025-05-06 08:37:32 +0200

    Carrousel images (secteurs d'activité)
    Ajout d'un carrousel d'image avec JS pour illustrer la section "Nos secteurs d'activité" de la page d'accueil du site.

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ document.addEventListener("DOMContentLoaded", () => {
   
     observer.observe(video);
   });
+
+  window.addEventListener("DOMContentLoaded", () => {
+    const track = document.getElementById("carousel-track");
+    const images = Array.from(track.children);
+    images.forEach(img => {
+      const clone = img.cloneNode(true);
+      track.appendChild(clone);
+    });
+  });
+  
+  function openImage(src) {
+    const lightbox = document.getElementById("lightbox");
+    const img = document.getElementById("lightbox-img");
+    img.src = src;
+    lightbox.classList.remove("hidden");
+    lightbox.classList.add("flex");
+  }
+  
+  function closeImage() {
+    const lightbox = document.getElementById("lightbox");
+    lightbox.classList.remove("flex");
+    lightbox.classList.add("hidden");
+  }
